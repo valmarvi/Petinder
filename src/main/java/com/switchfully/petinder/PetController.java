accfc9fddbb962e9b5b1449edb3d093fea29737b
valmarvi <valter.vianes@gmail.com>
2023-03-15 13:15:42 +0100

    10-spring-boot

--- src/main/java/com/switchfully/petinder/PetController.java
+++ src/main/java/com/switchfully/petinder/PetController.java
@@ -1,5 +1,7 @@
 package com.switchfully.petinder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,8 @@ import java.util.List;
 @RequestMapping(value="pets")
 public class PetController {
     private final PetService petService;
+
+    Logger logger = LoggerFactory.getLogger(PetController.class);
 
     public PetController(PetService petService) {
         this.petService = petService;
@@ -23,5 +27,15 @@ public class PetController {
     @GetMapping(produces = "application/json")
     public List<PetDTO> getAllPets() {
         return petService.getAllPets();
+    }
+
+    @RequestMapping("/logger")
+    public String helloExample() {
+        logger.trace("I'll update you on every heartbeat 💓");
+        logger.debug("I'm taking a (sub)step 🚶");
+        logger.info("Business as usual, I processed an action correctly");
+        logger.warn("Warning ⚠ I do not trust this situation?!");
+        logger.error("Help 🆘 Something is broken!");
+        return "<h1>`System.out.println();` is forbidden!</h1><p>Logging for the win!</p>";
     }
 }
